932970df606d6e8797dd31e18bd38535c2e25a10
ilanreinstein <ilanreinstein@gmail.com>
2017-03-02 09:54:47 -0500

    final results

--- cuip/plumes/IlanImgProc/get_time_series.py
+++ cuip/plumes/IlanImgProc/get_time_series.py
@@ -1,43 +1,13 @@
 import numpy as np
 import os
+import sys
 import os.path
 import glob
-im
 import pylab as pl
-import datetim as dt
-
-
-from PIL import Image
-
-
-home = '/projects/projects/project-uo_visible_plumes/workspace/share/plumes/'
-root = '/home/cusp/ir729/cuip/cuip/plumes/IlanImgProc/'
-
-sl_idx = np.load(root + 'skyline_idx.npy')
-city_idx  = np.load(root + 'city_idx.npy')
-sky_idx = np.load(root + 'sky_idx.npy')
-skyline = np.load(root + 'img1_skyline.npy')
-
-dir_gen = os.walk(home)
-
-path = glob.glob('*.raw')
-
-
-def img2D(file_path):
-	im = np.fromfile(file_path, np.uint8)
-	im = im.astype(float)
-	im *= 255 / im.max()
-	im2d = (im.reshape([2160, 4096, 3]).sum(2) / 3.)
-	im2d /= im2d.max()
-	return im2d
-
-
-#Still need tweaking and revision
-def get_index(list):
-	new_list = []
-	for item in list:
-		new_list.append(im2d[index[0], index[1]])
-        return new_list
+import datetime as dt
+import pylab as pl
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 #return a list of string with names of directories 
@@ -47,51 +17,91 @@ def get_dir_name(dir_path):
 		if os.path.isdir(item) == True:
 			dirs.append(item)
 	return dirs
-#generate a dictioary with the name of the subdirectories and the corresponding timestamp to each image
-def time_tree(generator):
+
+def img2D(file_name):
+	im = np.fromfile(file_name, np.uint8)
+	im = im.astype(float)
+	im *= 255 / im.max()
+	im2d = (im.reshape([2160, 4096, 3]).sum(2) / 3.)
+	im2d /= im2d.max()
+	return im2d
+
+def extract_images(path):
+	img_list = []
+	for filename in os.listdir(path):
+		img_list.append(img2D(os.path.join(path,filename)))
+	return img_list
+
+def extract_times(time_string):
 	n_photos = 40
-	time_tree = {}
-	for root_path, dirs, files in generator:
-		time = os.path.basename(root_path)
-		base = datetime.datetime.strptime(time, '%H.%M.%S')
-		time_tree[time] = [base + datetime.timedelta(seconds = i*10) for i in range(0,n_photos)]
-		
-	return time_tree
+	base = dt.datetime.strptime(time_string, '%H.%M.%S') 
+	time_vector = [base + dt.timedelta(seconds = i*10) for i in range(0,n_photos)]
+	return time_vector
 
+def prepare_data(rootdir, time_dirs):
 
-def get_images(generator):
-	path_tree = {}
-	for root_path, dirs, files in generator:
-		path_tree[os.path.basename(root_path)] = [os.path.join(root_path, file_path) for file_path in files]
-	return path_tree
+	image_list = []
+	time_list = []
+	for fldr in time_dirs:
+		home = os.path.join(rootdir, fldr)
+		image_list.append(extract_images(home))
+		time_list.append(extract_times(home))
 
-def read_img(dictionary):
-	img_tree = {}
-	for key in dictionary:
-		img_list = []
-		for item in dictionary[key]:
-			im = im2D(item)
-			img_list.append(im)
-		img_tree[key] = img_list
-		
-	return img_tree
+	time_list = [item for sublist in time_list for item in sublist]
+	#image_list = [item for sublist in image_list for item in sublist]
+	return image_list, time_list
 
+def filter_data(image_list, index):
+
+	temp_list = []
+	for item in image_list: 
+		for image in item:
+			temp_list.append(image[index].tolist())
+	
+	data = pd.DataFrame(temp_list)
+	data['times'] = times
+    		
+	return data
+
+def load_index(root):
+	sl_idx = np.load(root + 'skyline_idx.npy').tolist()
+	city_idx  = np.load(root + 'city_idx.npy').tolist()
+	sky_idx = np.load(root + 'sky_idx.npy').tolist()
+	
+	return sl_idx, city_idx, sky_idx
+	
 
 if __name__ == '__main__':
-	home = '/projects/projects/project-uo_visible_plumes/workspace/share/plumes/'
+	rootdir = '/projects/projects/project-uo_visible_plumes/workspace/share/plumes/'
 	root = '/home/cusp/ir729/cuip/cuip/plumes/IlanImgProc/'
+	
+	data_path = '/home/cusp/ir729/data/'
 
-	sl_idx = np.load(root + 'skyline_idx.npy')
-	city_idx  = np.load(root + 'city_idx.npy')
-	sky_idx = np.load(root + 'sky_idx.npy')
-	skyline = np.load(root + 'img1_skyline.npy')
-	dir_gen = os.walk(home)
+
+#	sl_idx, city_idx, sky_idx = load_index(root)
+	
+#	time_dirs = get_dir_name(rootdir)
+	#time_dirs.sort()
+
+	#image_list, time_list =	prepare_data(rootdir, time_dirs)
+	
+	sky = pd.read_csv(data_path + 'data_sky.csv')
+	skyline = pd.read_csv(data_path + 'data_skyline.csv')
+	city = pd.read_csv(data_path + 'data_city.csv')
+
+	sky.plot(x = sky.times, subplots = True, figsize = (9, 12), title = 'Sky')
+	skyline.plot(x = skyline.times, subplots = True, figsize = (9, 12), title = 'Skyline')
+	city.plot(x = city.times, subplots = True, figsize = (9, 12), title = 'City')
+
+	sky.plot.hist(subplots = True, figsize = (9, 12), title = 'Sky', bins = 30)
+	skyline.plot.hist(subplots = True, figsize = (9, 12), title = 'Skyline', bins = 30)
+	city.plot.hist(subplots = True, figsize = (9, 12), title = 'City', bins = 30)
+	plt.show()
+	
+	
+
+	
 
 	
 	
-	t_dict =  time_tree(dir_gen)
-	img_dict = get_images(dir_gen)
-	img_vector = read_img(img_dict)
 
-#At this point I should still get the indexes to each image and then merge both dictionaries 
-#in such a way that I hava dataframe that I can use to create the timeseries
