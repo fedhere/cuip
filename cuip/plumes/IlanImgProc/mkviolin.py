b9da275cb425a69e7174f5816477080da08f37ae
Federica Bianco <fbianco@compute.cusp.nyu.edu>
2017-03-05 00:13:14 -0500

    adding violin plot

---
+++ cuip/plumes/IlanImgProc/mkviolin.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import get_time_seriesFed as gt
+import pylab as pl
+
+NPTS=10
+skyline =np.load("img1_skyline.npy")
+sk, sl, ct = gt.getts(skyline, n=NPTS, lim=-1)
+all3 = []
+concatcols  = ["times","scene"]
+
+for i in range(NPTS):
+        all3 += [sk[concatcols + ["mean%03d"%i]]]
+        all3 += [ct[concatcols + ["mean%03d"%i]]]
+
+tmp = pd.concat(all3)
+tmp["whichdp"] = np.where(~np.isnan(tmp[["mean%03d"%i for i in range(NPTS)]]))[1]
+tmp["mean"] = np.nansum(tmp[["mean%03d"%i for i in range(NPTS)]], axis=1)
+
+flatui = ["#9b59b6", "#3498db", "#95a5a6","#dcdcc6"]
+sns.set_palette(flatui)
+sns.violinplot(x="whichdp",y="mean", data=tmp, split=True, hue="scene")
+pl.show()
+raw_input()
