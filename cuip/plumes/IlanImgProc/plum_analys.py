c7e1a6879bb78673f19a7cfffe6fbbc29abb0b23
ilanreinstein <ilanreinstein@gmail.com>
2017-03-26 17:01:54 -0400

    added dataframes for analysis

--- cuip/plumes/IlanImgProc/plum_analys.py
+++ cuip/plumes/IlanImgProc/plum_analys.py
@@ -7,37 +7,63 @@ npy_path = os.path.join(plumes, 'outputs') #dir with np arrays for anlysis
 root = os.getenv('CUIP') # plumes repo
 
 
-THRESHOLD = 60
+
+#select images with clear plumes
+plumes_med = ymed_log[8:12]
+plumes_mdif = ymdif_log[8:12]
+
+#empty vectors to store the gaussian fitting. 
+ymed_fit = np.zeros((len(plumes_med), BINS))
+ymdif_fit = np.zeros((len(plumes_mdif), BINS))
+
+#initializing mean and sigma
+mu_med = np.zeros((len(plumes_med)))
+mu_mdif = np.zeros((len(plumes_med)))
+sigma_med = np.zeros((len(plumes_mdif)))
+sigma_mdif = np.zeros((len(plumes_mdif)))
 
 
-def dist_expl(array_file, threshold = THRESHOLD, box = False, hist = False):
-    '''
-    
-    Input: array file for distribution analysis after threshold filtering
-    box: plots box plot of flat array, default = False
-    hist: plots histogram of flat array, default = False
-    
-    '''
+###MEDIAN.NPY fitting
 
-    f = np.load(array_file)
-    f = f.flatten()
-    out = f[f > THRESHOLD] #cut of values
+#calculate mean and sigma for each image dist. for first guess in curve fitting
+for i in range(len(plumes_med)):
+    mu_med[i] = sum(bins_cen*plumes_med[i])/sum(plumes_med[i])
+    sigma_med[i] = np.sqrt(sum(plumes_med[i]))
+
+#fitting for each image dist
+for i in range(len(plumes_med)):
+    ymed_fit[i] = gf.gaussfit(plumes_med[i], bins_cen, plumes_med[i].max(), mu_med[i], sigma_med[i])
+
+plt.ion()
+plt.figure()
+plt.subplot(111)
+
+#plotting of the histograms with respective fitted curve
+for i in range(len(plumes_med)):
+    plt.bar(bins_cen, plumes_med[i], alpha = 0.75)
+    plt.plot(bins_cen, ymed_fit[i], 'r-')
+    plt.draw()
+    raw_input()
+'''
+### FITTING AND PLOTTING FOR MINDIF.NPY. TO-DO
+
+#for i in range(len(plumes_mdif)):
+ #   mu_mdif[i] = sum(bins_cen*plumes_mdif[i])/sum(plumes_mdif[i])
+ #   sigma_mdif[i] = np.sqrt(sum(plumes_mdif[i]))
 
 
-    if box:
-        plt.boxplot(out)
-        plt.show()
-    
-    if hist:
-        plt.hist(out)
-        plt.show()
+#for i in range(len(plumes_mdif)):
+#    ymed_fit[i] = gf.gaussfit(plumes_mdif[i], bins_cen, plumes_mdif[i].max(), mu_mdif[i], sigma_mdif[i])
 
 
-    
+#plt.figure(2)
+#plt.subplot(111)
 
-if __name__ == '__main__':
-    
-    f = 'tmp_0147_median.npy'
-    file_path = os.path.join(npy_path, f)
-    dist_expl(file_path, box = True)
-    dist_expl(file_path, hist = True)
+#for i in range(len(plumes_mdif)):
+ #   plt.bar(bins_cen, plumes_mdif[i], alpha = 0.75)
+  #  plt.plot(bins_cen, ymed_fit[i], 'r-')
+   # plt.draw()
+    #raw_input()
+
+
+
--- cuip/plumes/test10imgs.py
+++ cuip/plumes/test10imgs.py
@@ -2,113 +2,67 @@ import glob
 import os
 import sys
 import numpy as np
-import scipy.stats as sts
 import matplotlib.pyplot as plt
+import pandas as pd
 
 PLUMES = os.getenv('PLUMES_DATA')
 sys.path.append(PLUMES)
 import gaussfit as gf
 
 
-### Read in 20 images (140 to 159) from both median.npy and mindif.npy and store in arrays.
+### Read in 30 images (130 to 159) from both median.npy and mindif.npy and store in arrays.
 ### Create histogram objects and extract X and Y values for each image
 
 
 #empty array to catch read images
-img_med = np.zeros((20, 1300, 4096))
-#img_mdif = np.zeros((20, 1300, 4096))
+img_med = np.zeros((30, 1300, 4096))
+img_mdif = np.zeros((30, 1300, 4096))
 
-for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES,'outputs/tmp_01[45][0-9]_median.npy')))):
+for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES,'outputs/tmp_01[345][0-9]_median.npy')))):
     print(f)
     img_med[i] = np.load(f)[200:1500,:,:].mean(-1)
 
-#for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES, 'outputs/tmp_01[45][0-9]_mindif.npy')))):
-#    print(f)
-#    img_mdif[i] = np.load(f)[200:1500,:,:].mean(-1)
+for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES, 'outputs/tmp_01[345][0-9]_mindif.npy')))):
+    print(f)
+    img_mdif[i] = np.load(f)[200:1500,:,:].mean(-1)
 
 #set number of bins to 64 or Rice method
 #save the X and Y (bin heights) components from histograms
 
-BINS = 64   #int(round(2*len(img_med[0].flatten())**(1./3.)))
-y_med = np.zeros((20, BINS))
-#y_mdif = np.zeros((20, BINS))
+BINS = int(round(2*len(img_med[0].flatten())**(1./3.)))
+y_med = np.zeros((30, BINS))
+y_mdif = np.zeros((30, BINS))
 x = np.zeros(BINS)
 patches = np.zeros(BINS) #third element of histogram tuple NOT USED
 
 
-for i in range(20):
+for i in range(30):
     y_med[i], x, patches = plt.hist(img_med[i].flatten(), bins = BINS, log = True)
     y_mdif[i] = plt.hist(img_mdif[i].flatten(), bins = BINS, log = True)[0]
     
 
 #Log10 of the bin heights
 ymed_log = np.log10(y_med)
-#ymdif_log = np.log10(y_mdif)
+ymdif_log = np.log10(y_mdif)
 
 
 #set -inf to zero
 ymed_log[ymed_log == -np.inf] = 0.0
-#ymdif_log[ymdif_log == -np.inf] = 0.0
+ymdif_log[ymdif_log == -np.inf] = 0.0
 
 #create new X axis of the same dimension as number of bins. See doc on histograms
 bins_cen = x[:-1] + 0.5*(x[1]-x[0])
 
-#select images with clear plumes
-plumes_med = ymed_log[8:12]
-#plumes_mdif = ymdif_log[8:12]
+#Create two new dataframes for simplicity. Already saved in IlanImgProc/
 
-#empty vectors to store the gaussian fitting. 
-ymed_fit = np.zeros((len(plumes_med), BINS))
-#ymdif_fit = np.zeros((len(plumes_mdif), BINS))
+cols = ['img_%d'%(i) for i in range(130,160)]
 
-#initializing mean and sigma
-mu_med = np.zeros((len(plumes_med)))
-#mu_mdif = np.zeros((len(plumes_med)))
-sigma_med = np.zeros((len(plumes_mdif)))
-#sigma_mdif = np.zeros((len(plumes_mdif)))
+median_data = pd.DataFrame(ymed_log.T, columns = cols)
+median_data.insert(0, 'X', bins_cen)
+pd.DataFrame.to_csv(median_data, 'IlanImgProc/median_data.csv')
 
 
-###MEDIAN.NPY fitting
-
-#calculate mean and sigma for each image dist. for first guess in curve fitting
-for i in range(len(plumes_med)):
-    mu_med[i] = sum(bins_cen*plumes_med[i])/sum(plumes_med[i])
-    sigma_med[i] = np.sqrt(sum(plumes_med[i]))
-
-#fitting for each image dist
-for i in range(len(plumes_med)):
-    ymed_fit[i] = gf.gaussfit(plumes_med[i], bins_cen, plumes_med[i].max(), mu_med[i], sigma_med[i])
-
-plt.ion()
-plt.figure()
-plt.subplot(111)
-
-#plotting of the histograms with respective fitted curve
-for i in range(len(plumes_med)):
-    plt.bar(bins_cen, plumes_med[i], alpha = 0.75)
-    plt.plot(bins_cen, ymed_fit[i], 'r-')
-    plt.draw()
-    raw_input()
-
-### FITTING AND PLOTTING FOR MINDIF.NPY. TO-DO
-
-#for i in range(len(plumes_mdif)):
- #   mu_mdif[i] = sum(bins_cen*plumes_mdif[i])/sum(plumes_mdif[i])
- #   sigma_mdif[i] = np.sqrt(sum(plumes_mdif[i]))
-
-
-#for i in range(len(plumes_mdif)):
-#    ymed_fit[i] = gf.gaussfit(plumes_mdif[i], bins_cen, plumes_mdif[i].max(), mu_mdif[i], sigma_mdif[i])
-
-
-#plt.figure(2)
-#plt.subplot(111)
-
-#for i in range(len(plumes_mdif)):
- #   plt.bar(bins_cen, plumes_mdif[i], alpha = 0.75)
-  #  plt.plot(bins_cen, ymed_fit[i], 'r-')
-   # plt.draw()
-    #raw_input()
-
-
+mindif_data = pd.DataFrame(ymdif_log.T, columns = cols)
+mindif_data.insert(0, 'X', bins_cen)
+pd.DataFrame.to_csv(mindif_data, 'IlanImgProc/mindif_data.csv')
 
