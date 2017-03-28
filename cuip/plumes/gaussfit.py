90d3a7a27fa9de41dfec86892d92ced49830daed
IlanReinstein <ilanreinstein@gmail.com>
2017-03-28 13:53:16 -0400

    latest analysis script

--- cuip/plumes/gaussfit.py
+++ cuip/plumes/gaussfit.py
@@ -7,4 +7,18 @@ def gauss(x, *p):
 
 def gaussfit(datay, datax, A, mu, sig):
     coeff, var = curve_fit(gauss, datax, datay, p0=[A, mu, sig])
-    return gauss(datax, *coeff)
+    return gauss(datax, *coeff), coeff
+
+def lorentzian(x, *p):
+	A, x0 = p
+	return A / ((x-x0)**2 + A**2) / np.pi
+
+def lorentzfit(datay, datax, A, x0):
+	coeff, var = curve_fit(lorentzian, datax, datay, p0 = [A, x0])
+	return lorentzian(datax, *coeff)  #, coeff[0], coeff[1]
+
+
+#Lorentzian first guess
+#a_guess = 1 / (np.pi * max(ydata))
+#x0_guess = sum(xdata * ydata) / sum(ydata)
+
--- cuip/plumes/plum_analys.py
+++ cuip/plumes/plum_analys.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import gaussfit as gf
 
+
 img_med = pd.read_csv('IlanImgProc/median_data.csv', index_col = 0) #median dataframe
 img_mdif = pd.read_csv('IlanImgProc/mindif_data.csv', index_col = 0) #mindif dataframe
 
@@ -16,14 +17,37 @@ cols = ['img%d'%(i) for i in range(130,160)] #image ID vector
 
 ###MEDIAN.NPY fitting
 
-med_fit = pd.DataFrame(index = range(nrows), columns = cols)  #empty dataframe to save fitted curves by image
+medfit_g = pd.DataFrame(index = range(nrows), columns = cols)  #empty dataframe to save fitted curves by image
+var_g = np.zeros((30, 3))
 
+
+#Gaussfit
 for i in range(1,ncols):
     try:
-        med_fit.iloc[:,i-1] = gf.gaussfit(img_med.iloc[:,i], img_med.X, img_med.iloc[:,i].max(), 0, 10) \
-            #img_med.iloc[:,i].mean(), img_med.iloc[:,i].std())
+        medfit_g.iloc[:,i-1], var_g[i-1]= gf.gaussfit(img_med.iloc[:,i], img_med.X, img_med.iloc[:,i].max(), \
+            img_med.iloc[:,i].mean(), img_med.iloc[:,i].std())
     except RuntimeError: #because the fitting will not work for all images run through all to find those that don't work
         continue
+    #var[i-1] = gf.gaussfit(img_med.iloc[:,i], img_med.X, img_med.iloc[:,i].max(), \
+    #        img_med.iloc[:,i].mean(), img_med.iloc[:,i].std())[1]
+
+
+#Lorentzfit
+
+medfit_l = pd.DataFrame(index = range(nrows), columns = cols) 
+var_l = np.zeros((30,2))
+
+a = lambda x: 1 / (np.pi *max(x))
+x0 = lambda x: sum(img_med.X * x) / sum(x)
+
+for i in range(1,ncols):
+    #try:
+    medfit_l.iloc[:,i-1] = gf.lorentzfit(img_med.iloc[:,i], img_med.X, a(img_med.iloc[:,i]), \
+            x0(img_med.iloc[:,i]))
+    #except RuntimeError: 
+    #    continue
+
+
 
 ###MINDIF.NPY fitting
 
