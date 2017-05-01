523a47a5df64fad56e87e8c1fba1780a02044a16
IlanReinstein <ilanreinstein@gmail.com>
2017-05-01 14:08:45 -0400

    adding txt outputs

--- cuip/plumes/plum_analys.py
+++ cuip/plumes/plum_analys.py
@@ -13,92 +13,92 @@ img_mdif = pd.read_csv('IlanImgProc/mindif_data.csv', index_col = 0) #mindif dat
 
 
 nrows, ncols = img_med.shape
-cols = ['img%d'%(i) for i in range(130,160)] #image ID vector
+#cols = ['img%d'%(i) for i in range(130,160)] #image ID vector
 
 
 ###MEDIAN.NPY fitting
 
-mdfit = pd.DataFrame(index = range(nrows), columns = cols)  #empty dataframe to save fitted curves by image
-mdcoeff= np.zeros((30, 3))
+#mdfit = pd.DataFrame(index = range(nrows), columns = cols)  #empty dataframe to save fitted curves by image
+#mdcoeff= np.zeros((30, 3))
 
 
 #Gaussfit
-for i in range(1,ncols):
-    try:
-        mdfit.iloc[:,i-1], mdcoeff[i-1]= gf.gaussfit(img_med.iloc[:,i], img_med.X, img_med.iloc[:,i].max(), \
-            img_med.iloc[:,i].mean(), img_med.iloc[:,i].std())
-    except RuntimeError: #because the fitting will not work for all images run through all to find those that don't work
-        mdcoeff[i-1] = [img_med.iloc[:,i].max(), 0, img_med.iloc[:,i].std()]
+#for i in range(1,ncols):
+#    try:
+#        mdfit.iloc[:,i-1], mdcoeff[i-1]= gf.gaussfit(img_med.iloc[:,i], img_med.X, img_med.iloc[:,i].max(), \
+#            img_med.iloc[:,i].mean(), img_med.iloc[:,i].std())
+#    except RuntimeError: #because the fitting will not work for all images run through all to find those that don't work
+#        mdcoeff[i-1] = [img_med.iloc[:,i].max(), 0, img_med.iloc[:,i].std()]
 
 
 ###MINDIF.NPY fitting
 
 
-mnfit = pd.DataFrame(index = range(nrows), columns = cols) #empty dataframe to save fitted curves by image
-mncoeff = np.zeros((30, 3))
-for i in range(1, ncols):
-    try:
-        mnfit.iloc[:,i-1], mncoeff[i-1] =  gf.gaussfit(img_mdif.iloc[:,i], img_mdif.X, img_mdif.iloc[:,i].max(), \
-                    img_mdif.iloc[:,i].mean(), img_mdif.iloc[:,i].std())
-    except RuntimeError:
-        mncoeff[i-1]  = [img_mdif.iloc[:,i].max(), 0, img_mdif.iloc[:,i].std()]
+#mnfit = pd.DataFrame(index = range(nrows), columns = cols) #empty dataframe to save fitted curves by image
+#mncoeff = np.zeros((30, 3))
+#for i in range(1, ncols):
+#    try:
+#        mnfit.iloc[:,i-1], mncoeff[i-1] =  gf.gaussfit(img_mdif.iloc[:,i], img_mdif.X, img_mdif.iloc[:,i].max(), \
+#                    img_mdif.iloc[:,i].mean(), img_mdif.iloc[:,i].std())
+#    except RuntimeError:
+#        mncoeff[i-1]  = [img_mdif.iloc[:,i].max(), 0, img_mdif.iloc[:,i].std()]
 
 
 
 #real_img = img_med.drop('X', axis = 1) #filtering the X columnn (not an image)
 
 #selecting the images with plumes and those without
-mask =  np.ones(ncols-1, dtype=bool)
-mask[12:23] = False
+#mask =  np.ones(ncols-1, dtype=bool)
+#mask[12:23] = False
 
 ###Sigma values from original ditribution
 #median
-md_std = img_med.iloc[:,1:].std()
+#md_std = img_med.iloc[:,1:].std()
 
-md_nplum = md_std[mask]
-md_plum = md_std[~mask]
+#md_nplum = md_std[mask]
+#md_plum = md_std[~mask]
 
 #mindif
-mn_std = img_mdif.iloc[:,1:].std()
+#mn_std = img_mdif.iloc[:,1:].std()
 
-mn_nplum = mn_std[mask]
-mn_plum = mn_std[~mask]
+#mn_nplum = mn_std[mask]
+#mn_plum = mn_std[~mask]
 
 
 ###Values from fitted distribution
 #median
 
-md_fitsd = mdcoeff[:,2]
-mdfit_nplm = md_fitsd[mask]
-mdfit_plm = md_fitsd[~mask]
+#md_fitsd = mdcoeff[:,2]
+#mdfit_nplm = md_fitsd[mask]
+#mdfit_plm = md_fitsd[~mask]
 
 #mindif
-mn_fitsd = mncoeff[:,2]
-mnfit_nplm = mn_fitsd[mask]
-mnfit_plm = mn_fitsd[~mask]
+#mn_fitsd = mncoeff[:,2]
+#mnfit_nplm = mn_fitsd[mask]
+#mnfit_plm = mn_fitsd[~mask]
 
 ###Plotting sigma for real distributions
 #median
-pl.ion()
-fig, (ax1, ax2) = pl.subplots(2)
-ax1.set_title('Real Image Variance (median)')
-sns.set_style('whitegrid')
-sns.distplot(md_nplum, hist = False, label = 'No Plume Sigmas', ax = ax1)
-sns.distplot(md_plum, hist = False, label = 'Plume Sigmas', ax = ax1)
+#pl.ion()
+#fig, (ax1, ax2) = pl.subplots(2)
+#ax1.set_title('Real Image Variance (median)')
+#sns.set_style('whitegrid')
+#sns.distplot(md_nplum, hist = False, label = 'No Plume Sigmas', ax = ax1)
+#sns.distplot(md_plum, hist = False, label = 'Plume Sigmas', ax = ax1)
 
 #mindif
-ax2.set_title('Real Image Variance (mindif)')
-sns.distplot(mn_nplum, hist = False, label = 'No Plume Sigmas', ax = ax2)
-sns.distplot(mn_plum, hist = False, label = 'Plume Sigmas', ax = ax2)
-pl.show(block = True)
+#ax2.set_title('Real Image Variance (mindif)')
+#sns.distplot(mn_nplum, hist = False, label = 'No Plume Sigmas', ax = ax2)
+#sns.distplot(mn_plum, hist = False, label = 'Plume Sigmas', ax = ax2)
+#pl.show(block = True)
 
 
 ###Plotting for fitted distributions
 #mindif
-plt.figure()
-plt.title('Fitted Image Variance comparison (median)')
-sns.distplot(fit_nplm, hist = False, label = 'No Plume Sigmas')
-sns.distplot(fit_plm, hist = False, label = 'Plume Sigmas')
+#plt.figure()
+#plt.title('Fitted Image Variance comparison (median)')
+#sns.distplot(fit_nplm, hist = False, label = 'No Plume Sigmas')
+#sns.distplot(fit_plm, hist = False, label = 'Plume Sigmas')
 
 
 
--- cuip/plumes/test10imgs.py
+++ cuip/plumes/test10imgs.py
@@ -12,68 +12,57 @@ import gaussfit as gf
 
 ### Read in 30 images (130 to 159) from both median.npy and mindif.npy and store in arrays.
 ### Create histogram objects and extract X and Y values for each image
-imgpath1 =  os.path.join(PLUMES,'outputs/tmp_*_median.npy')
-imgpath2 =  os.path.join(PLUMES,'outputs/tmp_*_mindif.npy')
 
-#print imgpath
-imglist1 = sorted(glob.glob(imgpath1))
-imglist2 = sorted(glob.glob(imgpath2))
-        
-
-#print imglist
-nimgs = len(imglist1)
 
 #empty array to catch read images
-imgs = np.zeros((nimgs, 1300, 4096))
-    
-imgs_min = np.zeros((nimgs, 1300, 4096))
+img_med = np.zeros((30, 1300, 4096))
+img_mdif = np.zeros((30, 1300, 4096))
 
-
-for i,f in enumerate(imglist1):
+for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES,'outputs/tmp_01[345][0-9]_median.npy')))):
     print(f)
-    imgs[i] = np.load(f)[200:1500,:,:].mean(-1)
+    img_med[i] = np.load(f)[200:1500,:,:].mean(-1)
 
-for i,f in enumerate(imglist2):
+for i,f in enumerate(sorted(glob.glob(os.path.join(PLUMES, 'outputs/tmp_01[345][0-9]_mindif.npy')))):
     print(f)
-    imgs_min[i] = np.load(f)[200:1500,:,:].mean(-1)
+    img_mdif[i] = np.load(f)[200:1500,:,:].mean(-1)
 
 #set number of bins to 64 or Rice method
 #save the X and Y (bin heights) components from histograms
 
-BINS = 60#int(round(2*len(img_med[0].flatten())**(1./3.)))
-y_md = np.zeros((nimgs, BINS))
-y_mn = np.zeros((nimgs, BINS))
+BINS = int(round(2*len(img_med[0].flatten())**(1./3.)))
+y_med = np.zeros((30, BINS))
+y_mdif = np.zeros((30, BINS))
 x = np.zeros(BINS)
 patches = np.zeros(BINS) #third element of histogram tuple NOT USED
 
 
-for i in range(nimgs):
-    y_md[i], x, patches = plt.hist(imgs[i].flatten(), bins = BINS, log = True)
-    y_mn[i] = plt.hist(imgs_min[i].flatten(), bins = BINS, log = True)[0]
+for i in range(30):
+    y_med[i], x, patches = plt.hist(img_med[i].flatten(), bins = BINS, log = True)
+    y_mdif[i] = plt.hist(img_mdif[i].flatten(), bins = BINS, log = True)[0]
     
 
 #Log10 of the bin heights
-ymd_log = np.log10(y_md)
-ymn_log = np.log10(y_mn)
+ymed_log = np.log10(y_med)
+ymdif_log = np.log10(y_mdif)
 
 
 #set -inf to zero
-ymd_log[np.isinf(ymd_log)] = 0.0
-ymn_log[np.isinf(ymn_log)] = 0.0
+ymed_log[ymed_log == -np.inf] = 0.0
+ymdif_log[ymdif_log == -np.inf] = 0.0
 
 #create new X axis of the same dimension as number of bins. See doc on histograms
 bins_cen = x[:-1] + 0.5*(x[1]-x[0])
 
 #Create two new dataframes for simplicity. Already saved in IlanImgProc/
 
-#cols = ['img%d'%(i) for i in range(nimgs)]
+cols = ['img_%d'%(i) for i in range(130,160)]
 
-#median_data = pd.DataFrame(ymd_log.T, columns = cols)
-#median_data.insert(0, 'X', bins_cen)
-#pd.DataFrame.to_csv(median_data, 'IlanImgProc/median_data.csv')
+median_data = pd.DataFrame(ymed_log.T, columns = cols)
+median_data.insert(0, 'X', bins_cen)
+pd.DataFrame.to_csv(median_data, 'IlanImgProc/median_data.csv')
 
 
-#mindif_data = pd.DataFrame(ymn_log.T, columns = cols)
-#mindif_data.insert(0, 'X', bins_cen)
-#spd.DataFrame.to_csv(mindif_data, 'IlanImgProc/mindif_data.csv')
+mindif_data = pd.DataFrame(ymdif_log.T, columns = cols)
+mindif_data.insert(0, 'X', bins_cen)
+pd.DataFrame.to_csv(mindif_data, 'IlanImgProc/mindif_data.csv')
 
